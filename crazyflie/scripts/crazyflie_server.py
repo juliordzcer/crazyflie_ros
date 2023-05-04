58b78d3afd149945efd69aa889c78ed8794bc096
juliordzcer <julio1811@outlook.es>
2023-05-04 16:44:48 -0600

    Se actualizaron los archivos

--- crazyflie/scripts/crazyflie_server.py
+++ crazyflie/scripts/crazyflie_server.py
@@ -2,36 +2,29 @@
 
 import rospy
 from geometry_msgs.msg import Twist, PoseStamped
-from sensor_msgs.msg import Imu, Temperature, MagneticField
-from std_msgs.msg import Float32
 from crazyflie.srv import *
 from std_srvs.srv import Empty, EmptyResponse
 
 import numpy as np
-import logging
-import sys
-import time
-import math
-from threading import Thread
 
 # Importando las librerias de python de bitcraze.
 import cflib
 import cflib.crtp
 from cflib.crazyflie import Crazyflie
 from cflib.crazyflie.log import LogConfig
-from cflib.utils import power_switch
+from threading import Thread
+
 
 class CrazyflieROS:
     Disconnected = 0
     Connecting = 1
     Connected = 2
 
-    def __init__(self, link_uri, tf_prefix, roll_trim, pitch_trim, enable_logging_imu ,enable_logging_pose):
+    def __init__(self, link_uri, tf_prefix, roll_trim, pitch_trim ,enable_logging_pose):
         self.link_uri = link_uri
         self.tf_prefix = tf_prefix
         self.roll_trim = roll_trim
         self.pitch_trim = pitch_trim
-        self.enable_logging_imu = enable_logging_imu
         self.enable_logging_pose = enable_logging_pose
         self._cf = Crazyflie()
 
@@ -57,9 +50,6 @@ class CrazyflieROS:
         self._isEmergency = False
 
         Thread(target=self._update).start()
-
-
-    # El comando open_link funciona para establecer la conexion con el crazyflie.
 
     def _try_to_connect(self):
         rospy.loginfo("Connecting to %s" % self.link_uri)
@@ -115,35 +105,27 @@ class CrazyflieROS:
 
 
     def _connection_failed(self, link_uri, msg):
-        """Callback when connection initial connection fails (i.e no Crazyflie
-        at the speficied address)"""
         rospy.logfatal("Connection to %s failed: %s" % (link_uri, msg))
+        self._cf.close_link()
         self._state = CrazyflieROS.Disconnected
 
     def _connection_lost(self, link_uri, msg):
-        """Callback when disconnected after a connection has been made (i.e
-        Crazyflie moves out of range)"""
         rospy.logfatal("Connection to %s lost: %s" % (link_uri, msg))
         self._state = CrazyflieROS.Disconnected
 
     def _disconnected(self, link_uri):
-        """Callback when the Crazyflie is disconnected (called in all cases)"""
         rospy.logfatal("Disconnected from %s" % link_uri)
         self._state = CrazyflieROS.Disconnected
 
     def _link_quality_updated(self, percentage):
-        """Called when the link driver updates the link quality measurement"""
         if percentage < 80:
             rospy.logwarn("Connection quality is: %f" % (percentage))
 
     def _log_error(self, logconf, msg):
-        """Callback from the log API when an error occurs"""
         rospy.logfatal("Error when logging %s: %s" % (logconf.name, msg))
 
 
     def _log_data_Pose(self, timestamp, data, logconf):
-        """Callback froma the log API when data arrives"""
-
         msg = Twist()
         msg.linear.x = data["stateEstimate.x"]
         msg.linear.y = data["stateEstimate.y"]
@@ -176,78 +158,48 @@ class CrazyflieROS:
         self._cf.loc.send_emergency_stop()
         return EmptyResponse()
 
-    def _send_setpoint(self):
-        roll    = self._cmdVel.linear.y + self.roll_trim
-        pitch   = self._cmdVel.linear.x + self.pitch_trim
-        yawrate = self._cmdVel.angular.z
-        thrust  = min(max(0, int(self._cmdVel.linear.z)), 59000)
-        #print(roll, pitch, yawrate, thrust)
+    def _cmdVelChanged(self, msg):
+        roll    = msg.linear.y + self.roll_trim
+        pitch   = msg.linear.x + self.pitch_trim
+        yawrate = msg.angular.z
+        thrust  = min(max(0, int(msg.linear.z)), 60000)
         self._cf.commander.send_setpoint(roll, pitch, yawrate, thrust)
-    
-    def _cmdVelChanged(self, data):
-        self._cmdVel = data
-        if not self._isEmergency:
-            self._send_setpoint()
 
-
-    # Send external pose.
     def _ExtPoseChanged(self, msg):
-
-        self._extpos = msg
-        
-        x = self._extpos.pose.position.x
-        y = self._extpos.pose.position.y
-        z = self._extpos.pose.position.z
-        quat = self._extpos.pose.orientation
+        x = msg.pose.position.x
+        y = msg.pose.position.y
+        z = msg.pose.position.z
+        quat = msg.pose.orientation
         if isnan(quat.x):
             self._cf.extpos.send_extpos(x, y, z)
         else:
             self._cf.extpos.send_extpose(x, y, z, quat.x, quat.y, quat.z, quat.w)
-        
-
-
-
 
     def _update(self):
         while not rospy.is_shutdown():
             if self._isEmergency:
                 break
-
             if self._state == CrazyflieROS.Disconnected:
                 self._try_to_connect()
             elif self._state == CrazyflieROS.Connected:
-                # Crazyflie will shut down if we don't send any command for 500ms
-                # Hence, make sure that we don't wait too long
-                # However, if there is no connection anymore, we try to get the flie down
-                # if self._subCmdVel.get_num_connections() > 0:
-                #     self._send_setpoint()
-                # else:
-                #     self._cmdVel = Twist()
-                #     self._send_setpoint()
                 rospy.sleep(0.2)
             else:
                 rospy.sleep(0.5)
-        for i in range(0, 100):
-            self._cf.commander.send_setpoint(0, 0, 0, 0)
-            rospy.sleep(0.01)
-        # Make sure that the last packet leaves before the link is closed
-        # since the message queue is not flushed before closing
         rospy.sleep(0.1)
         self._cf.close_link()
 
 
 
-
 def add_crazyflie(req):
-    rospy.loginfo("Adding %s as %s with trim(%f, %f). Logging_imu: %s, Logging_pose: %s" % (req.uri, req.tf_prefix, req.roll_trim, req.pitch_trim, str(req.enable_logging_imu), str(req.enable_logging_pose)))
-    CrazyflieROS(req.uri, req.tf_prefix, req.roll_trim, req.pitch_trim, req.enable_logging_imu, req.enable_logging_pose)
+    rospy.loginfo("Adding %s as %s with trim(%f, %f). Logging_pose: %s" % (req.uri, req.tf_prefix, req.roll_trim, req.pitch_trim, str(req.enable_logging_pose)))
+    CrazyflieROS(req.uri, req.tf_prefix, req.roll_trim, req.pitch_trim, req.enable_logging_pose)
     return AddCrazyflieResponse()
 
 if __name__ == '__main__':
     rospy.init_node('crazyflie_server')
 
     # Initialize the low-level drivers (don't list the debug drivers)
-    cflib.crtp.init_drivers(enable_debug_driver=False)
+    cflib.crtp.init_drivers()
 
     rospy.Service("add_crazyflie", AddCrazyflie, add_crazyflie)
     rospy.spin()
--- crazyflie_demo/scripts/publish_external_position.py
+++ crazyflie_demo/scripts/publish_external_position.py
@@ -1,35 +1,106 @@
 #!/usr/bin/env python3
 
 import rospy
-import tf
-from geometry_msgs.msg import PointStamped, TransformStamped, PoseStamped #PoseStamped added to support vrpn_client
+import math as m
+from geometry_msgs.msg import PoseStamped
 from crazyflie_driver.srv import UpdateParams
+import tf.transformations as t
 
-def onNewTransform(self, pose):
-    global firstTransform
+class ExternalPosition:
+    def __init__(self):
+        topic = rospy.get_param("~topic", "vrpn_client_node/crazyflie/pose")
+        self.yaw_sub = rospy.Subscriber(topic, PoseStamped, self.Newtransform)
+        self.extpos_pub = rospy.Publisher('external_position', PoseStamped, queue_size=50)
+        self.num_transforms_received = 0
+        rospy.wait_for_service('update_params')
+        rospy.loginfo("found update_params service")
+        self.update_params = rospy.ServiceProxy('update_params', UpdateParams)
+        self.x = 0
+        self.y = 0
+        self.z = 0
 
-    if firstTransform:
-        # initialize kalman filter
-        rospy.set_param("kalman/initialX", pose.pose.position.x)
-        rospy.set_param("kalman/initialY", pose.pose.position.y)
-        rospy.set_param("kalman/initialZ", pose.pose.position.z)
-        update_params(["kalman/initialX", "kalman/initialY", "kalman/initialZ"])
+    def Newtransform(self, msg):
+        self.num_transforms_received += 1
 
-        rospy.set_param("kalman/resetEstimation", 1)
-        update_params(["kalman/resetEstimation"]) 
-        firstTransform = False
+        self.x = msg.pose.position.x
+        self.y = msg.pose.position.y
+        self.z = msg.pose.position.z
 
+        if self.num_transforms_received == 1:
+            # initialize kalman filter
+            rospy.set_param("kalman/initialX", self.x)
+            rospy.set_param("kalman/initialY", self.y)
+            rospy.set_param("kalman/initialZ", self.z)
+            self.update_params(["kalman/initialX", "kalman/initialY", "kalman/initialZ"])
+            rospy.set_param("kalman/resetEstimation", 1)
+            rospy.sleep(2) 
+            self.update_params(["kalman/resetEstimation"]) 
+            rospy.set_param("locSrv/extPosStdDev", 1e-3)
+            # rospy.set_param("locSrv/extQuatStdDev", 0.5e-1)
+            self.update_params(["locSrv/extPosStdDev"])
+
+
+
+            
+
+
+    def PubPosition(self):
+        Position = PoseStamped()
+        Position.pose.position.x = self.x
+        Position.pose.position.y = self.y
+        Position.pose.position.z = self.z
+        self.extpos_pub.publish(Position)
 
 if __name__ == '__main__':
-    rospy.init_node('publish_external_position', anonymous=True)
-    topic = rospy.get_param("~topic", "vrpn_client_node/crazyflie/pose")
+    rospy.init_node('extpos')
+    rospy.sleep(12)  # espera 20 segundos antes de iniciar
+    ext_pos = ExternalPosition()
 
-    rospy.wait_for_service('update_params')
-    rospy.loginfo("found update_params service")
-    update_params = rospy.ServiceProxy('update_params', UpdateParams)
+    # Mantener el nodo en ejecución
+    rate = rospy.Rate(100)
+    while not rospy.is_shutdown():
+        ext_pos.PubPosition()
+        rate.sleep()
 
-    firstTransform = True
 
-    rospy.Subscriber(topic, PoseStamped, onNewTransform)
 
-    rospy.spin()
+
+
+
+
+
+# #!/usr/bin/env python3
+
+# import rospy
+# import tf
+# from geometry_msgs.msg import PointStamped, TransformStamped, PoseStamped #PoseStamped added to support vrpn_client
+# from crazyflie_driver.srv import UpdateParams
+
+# def onNewTransform(self, pose):
+#     global firstTransform
+
+#     if firstTransform:
+#         # initialize kalman filter
+#         rospy.set_param("kalman/initialX", pose.pose.position.x)
+#         rospy.set_param("kalman/initialY", pose.pose.position.y)
+#         rospy.set_param("kalman/initialZ", pose.pose.position.z)
+#         update_params(["kalman/initialX", "kalman/initialY", "kalman/initialZ"])
+
+#         rospy.set_param("kalman/resetEstimation", 1)
+#         update_params(["kalman/resetEstimation"]) 
+#         firstTransform = False
+
+
+# if __name__ == '__main__':
+#     rospy.init_node('publish_external_position', anonymous=True)
+#     topic = rospy.get_param("~topic", "vrpn_client_node/crazyflie/pose")
+
+#     rospy.wait_for_service('update_params')
+#     rospy.loginfo("found update_params service")
+#     update_params = rospy.ServiceProxy('update_params', UpdateParams)
+
+#     firstTransform = True
+
+#     rospy.Subscriber(topic, PoseStamped, onNewTransform)
+
+#     rospy.spin()
