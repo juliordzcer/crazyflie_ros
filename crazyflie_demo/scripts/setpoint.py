f0f86a9ae5c8cfa38c548322d12d87e4db82d311
juliordzcer <julio1811@outlook.es>
2023-07-22 17:32:58 -0600

    A

--- crazyflie_demo/scripts/setpoint.py
+++ crazyflie_demo/scripts/setpoint.py
@@ -1,218 +1,13 @@
 #!/usr/bin/env python3
-
-# import rospy
-# import numpy as np
-# from std_msgs.msg import Bool
-# from crazyflie_driver.msg import Position
-# from geometry_msgs.msg import Twist
-# from sensor_msgs.msg import Joy
-
-# r = 0.0
-# h = 0.0
-# t = 0.0
-# rt = 100
-# gr = 0
-
-# button_pressed = False
-# start_time = rospy.Time()
-
-# def joy_callback(joy_msg):
-#     global r, h, t, button_pressed, gr, start_time
-
-#     if joy_msg.buttons[2] == 1 and not button_pressed:
-#         rospy.loginfo('Trayectoria iniciada')
-#         button_pressed = True
-#         r = rospy.get_param("~radio", 0.16)
-#         h = rospy.get_param("~altura", 0.4)
-#         t = 0.0
-#         gr = 10
-#         start_time = rospy.Time.now()  # Store the starting time
-#         boolean_pub.publish(True)  # Publish True to boolean_topic
-#     elif joy_msg.buttons[10] == 1 and button_pressed:
-#         rospy.loginfo('Trayectoria reiniciada')
-#         r = 0.0
-#         h = 0.0
-#         t = 0.0
-#         gr = 0
-#         button_pressed = False
-#         start_time = rospy.Time()  # Reset the starting time
-#         boolean_pub.publish(False)  # Publish False to boolean_topic
-
-# def trajectory_circle():
-#     global pub, r, h, t, rt
-
-#     rospy.sleep(1)  # Wait 1 second before starting the trajectory
-    
-#     rate = rospy.Rate(rt)  # rt Hz
-
-#     while not rospy.is_shutdown():
-#         current_time = rospy.Time.now()
-
-#         if button_pressed:  # Verificar si la trayectoria ha sido iniciada
-#             if not start_time.is_zero():
-#                 elapsed_time = (current_time - start_time).to_sec()
-#                 if elapsed_time >= 60:
-#                     msg = Twist()
-#                     msg.angular.x = 0
-#                     pub1.publish(msg)
-#                     break
-
-#             pose = Position()
-#             pose.header.seq = 0
-#             pose.header.frame_id = "world"
-
-#             w = np.pi / 5.
-#             p = 15
-
-#             x = r * (np.arctan(p) + np.arctan(t - p)) * np.cos(w * t)
-#             y = r * (np.arctan(p) + np.arctan(t - p)) * np.sin(w * t)
-#             z = (h / 2) * (1 + np.tanh(t - 5))
-#             yaw = 0
-
-#             pose.x = x
-#             pose.y = y
-#             pose.z = z
-#             pose.yaw = yaw
-
-#             pub.publish(pose)
-
-#             msg = Twist()
-#             msg.angular.x = 10
-#             pub1.publish(msg)
-
-#         t += 0.01
-#         rate.sleep()
-
-# if __name__ == '__main__':
-#     rospy.init_node('trajectory_circle', anonymous=True)
-#     xi = rospy.get_param("~xi", 0.0)
-#     yi = rospy.get_param("~yi", 0.0)
-#     zi = rospy.get_param("~zi", 0.0)
-
-#     pub = rospy.Publisher('cmd_position', Position, queue_size=1)
-#     pub1 = rospy.Publisher('goalacc', Twist, queue_size=1)
-#     boolean_pub = rospy.Publisher('boolean_topic', Bool, queue_size=1)  # Add the boolean publisher
-
-#     joy_sub = rospy.Subscriber('joy', Joy, joy_callback)
-
-#     trajectory_circle()
-
-
-
-
-# #!/usr/bin/env python3
-# from std_msgs.msg import Bool
-
-# import rospy
-# import numpy as np
-# from crazyflie_driver.msg import Position
-# from sensor_msgs.msg import Joy
-# import tf
-
-# r = 0.0
-# h = 0.0
-# t = 0.0
-# rt = 100
-# gr = 0
-
-# button_pressed = False
-# start_time = rospy.Time()
-
-# def joy_callback(joy_msg):
-#     global r, h, t, button_pressed, gr, start_time
-
-#     if joy_msg.buttons[2] == 1 and not button_pressed:
-#         rospy.loginfo('Trayectoria iniciada')
-#         button_pressed = True
-#         r = radio  # 0.16
-#         h = altura  # 0.4
-#         t = 0.0
-#         gr = 10
-#         start_time = rospy.Time.now()  # Store the starting time
-#         boolean_pub.publish(True)  # Publish True to boolean_topic
-#     elif joy_msg.buttons[10] == 1 and button_pressed:
-#         rospy.loginfo('Trayectoria reiniciada')
-#         r = 0.0
-#         h = 0.0
-#         t = 0.0
-#         gr = 0
-#         button_pressed = False
-#         start_time = rospy.Time()  # Reset the starting time
-#         boolean_pub.publish(False)  # Publish False to boolean_topic
-
-# def trajectory_circle():
-#     global pub, r, h, t, rt, xi, yi, zi, button_pressed
-
-#     rospy.sleep(1)  # Wait 1 second before starting the trajectory
-    
-#     rate = rospy.Rate(rt)  # rt Hz
-
-#     while not rospy.is_shutdown():
-#         current_time = rospy.Time.now()
-
-#         if button_pressed:  # Verificar si la trayectoria ha sido iniciada
-#             if not start_time.is_zero():
-#                 elapsed_time = (current_time - start_time).to_sec()
-#                 rospy.loginfo('Tiempo: ' + str(elapsed_time))
-#                 if elapsed_time >= 60:
-#                     # pose.x = xi
-#                     # pose.y = yi   
-#                     pose.z = max(zi * np.exp(-0.1 * (elapsed_time - 55)), 0)
-#                     pose.yaw = yaw
-#                     pub.publish(pose)
-#                     break
-
-#             pose = Position()
-#             pose.header.seq = 0
-#             pose.header.frame_id = "world"
-
-#             w = np.pi / 6.
-#             p = 15
-
-#             x = r * (np.arctan(p) + np.arctan(t - p)) * np.cos(w * t) + xi
-#             y = r * (np.arctan(p) + np.arctan(t - p)) * np.sin(w * t) + yi
-#             z = (h / 2) * (1 + np.tanh(t - 2.5)) + zi
-#             yaw = 0
-
-#             pose.x = x
-#             pose.y = y
-#             pose.z = z
-#             pose.yaw = yaw
-
-#             pub.publish(pose)
-
-#         t += 0.01
-#         rate.sleep()
-
-# if __name__ == '__main__':
-#     rospy.init_node('trajectory_circle', anonymous=True)
-#     xi = rospy.get_param("~xi", 0.0)
-#     yi = rospy.get_param("~yi", 0.0)
-#     zi = rospy.get_param("~zi", 0.0)
-
-#     altura = rospy.get_param("~h", 0.0)
-#     radio = rospy.get_param("~r", 0.0)
-
-#     pub = rospy.Publisher('cmd_position', Position, queue_size=1)
-#     boolean_pub = rospy.Publisher('boolean_topic', Bool, queue_size=1)  # Add the boolean publisher
-
-#     joy_sub = rospy.Subscriber('joy', Joy, joy_callback)
-
-#     trajectory_circle()
-#     rospy.spin()
-
-    
-
-
-from std_msgs.msg import Bool
-
-import rospy
 import numpy as np
-from crazyflie_driver.msg import Full
-from std_msgs.msg import Header
+import rospy
+import tf
+from std_msgs.msg import Header, Bool
 from geometry_msgs.msg import Twist
 from sensor_msgs.msg import Joy
-import tf
+from crazyflie_driver.msg import Full
+from scipy import io
+import os
 
 r = 0.0
 h = 0.0
@@ -222,6 +17,9 @@ gr = 0
 
 button_pressed = False
 start_time = rospy.Time()
+
+# List to store data for saving in .mat file
+data_to_save = []
 
 def joy_callback(joy_msg):
     global r, h, t, button_pressed, gr, start_time
@@ -234,7 +32,11 @@ def joy_callback(joy_msg):
         t = 0.0
         gr = 10
         start_time = rospy.Time.now()  # Store the starting time
-        boolean_pub.publish(True)  # Publish True to boolean_topic
+
+        # Clear the data list when starting a new trajectory
+        global data_to_save
+        data_to_save = []
+
     elif joy_msg.buttons[10] == 1 and button_pressed:
         rospy.loginfo('Trayectoria reiniciada')
         r = 0.0
@@ -243,10 +45,12 @@ def joy_callback(joy_msg):
         gr = 0
         button_pressed = False
         start_time = rospy.Time()  # Reset the starting time
-        boolean_pub.publish(False)  # Publish False to boolean_topic
+        start_msg = Twist()
+        start_msg.angular.x= 0
+        start_pub.publish(start_msg) 
 
 def trajectory_circle():
-    global pub, r, h, t, rt, xi, yi, zi, button_pressed
+    global pub, r, h, t, rt, xi, yi, zi, button_pressed, data_to_save
 
     rospy.sleep(1)  # Wait 1 second before starting the trajectory
     
@@ -260,6 +64,9 @@ def trajectory_circle():
                 elapsed_time = (current_time - start_time).to_sec()
                 # rospy.loginfo('Tiempo: ' + str(elapsed_time))
                 if elapsed_time >= 60:
+                    start_msg = Twist()
+                    start_msg.angular.x = 0
+                    start_pub.publish(start_msg) 
                     break
 
             full_msg = Full()
@@ -300,6 +107,12 @@ def trajectory_circle():
             full_msg.twist2.angular.y = dyaw
             full_msg.twist2.angular.z = ddyaw
 
+            start_msg = Twist()
+            start_msg.angular.x = 10
+            start_pub.publish(start_msg)
+            
+            data_to_save.append([x - xi, y - yi, z - zi, yaw])
+
             pub.publish(full_msg)
         rate.sleep()
 
@@ -316,11 +129,18 @@ if __name__ == '__main__':
     radio = rospy.get_param("~r", 0.0)
 
     pub = rospy.Publisher('cmd_full', Full, queue_size=1)
-    boolean_pub = rospy.Publisher('boolean_topic', Bool, queue_size=1)  # Add the boolean publisher
+    start_pub = rospy.Publisher('/start', Twist, queue_size=1)  # Add the boolean publisher
 
     joy_sub = rospy.Subscriber('joy', Joy, joy_callback)
 
     trajectory_circle()
+
+    # Save the data to the Experimentos directory
+    experiments_dir = os.path.expanduser('~/Experimentos')
+    if not os.path.exists(experiments_dir):
+        os.makedirs(experiments_dir)
+
+    file_path = os.path.join(experiments_dir, 'trajectory_data.mat')
+    io.savemat(file_path, {'data': np.array(data_to_save)})
+
     rospy.spin()
-
-
